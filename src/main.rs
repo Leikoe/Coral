74ec66ce3260fbc0fdea7c10a2c972c6046e538b
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2024-12-13 20:09:26 +0100

    world

--- src/main.rs
+++ src/main.rs
@@ -19,6 +19,12 @@ use trackable::Trackable;
 
 const CONTROL_PERIOD: Duration = Duration::from_millis(50);
 
+#[derive(Clone)]
+pub struct World {
+    pub ball: Ball,
+    pub team: HashMap<RobotId, Robot>,
+}
+
 #[derive(Debug)]
 pub struct RobotCommand {
     vel: Vec2,
@@ -40,7 +46,7 @@ fn take_next_commands(robots: &mut HashMap<RobotId, Robot>) -> HashMap<RobotId, 
         .collect()
 }
 
-fn launch_control_thread(mut team: HashMap<RobotId, Robot>, ball: Ball) -> JoinHandle<()> {
+fn launch_control_thread(mut world: World) -> JoinHandle<()> {
     let mut interval = tokio::time::interval(CONTROL_PERIOD);
     tokio::spawn(async move {
         // Robot control loop, 1Hz
@@ -48,8 +54,8 @@ fn launch_control_thread(mut team: HashMap<RobotId, Robot>, ball: Ball) -> JoinH
             interval.tick().await; // first tick ticks immediately that's why it's at the beginning
 
             println!("[DEBUG] world state");
-            println!("\tball pos: {:?}", ball.get_pos());
-            for r in team.values() {
+            println!("\tball pos: {:?}", world.ball.get_pos());
+            for r in world.team.values() {
                 println!(
                     "\trobot {} | is_dribbling: {} | pos: {:?}",
                     r.get_id(),
@@ -60,10 +66,10 @@ fn launch_control_thread(mut team: HashMap<RobotId, Robot>, ball: Ball) -> JoinH
 
             println!("[DEBUG] sending commands!\n");
             // take the commands & apply them (simulate real robot)
-            take_next_commands(&mut team)
+            take_next_commands(&mut world.team)
                 .drain()
                 .for_each(|(rid, command)| {
-                    let r = team.get_mut(&rid).unwrap(); // TODO: check we have the robot to send the order to
+                    let r = world.team.get_mut(&rid).unwrap(); // TODO: check we have the robot to send the order to
                     r.apply_vel(command.vel);
                 });
         }
@@ -85,29 +91,31 @@ fn make_ball_spin(ball: Ball, timeout: Option<Duration>) -> JoinHandle<()> {
 /// Simulation of a real control loop
 #[tokio::main]
 async fn main() {
-    let ball = Ball::new(Point2::new(0.6, 0.)); // right in front of the goal
-    let mut team: HashMap<RobotId, Robot> = HashMap::new();
-    team.insert(0, Robot::new(0, Point2::zero()));
-    team.insert(1, Robot::new(1, Point2::zero()));
-    team.insert(2, Robot::new(2, Point2::zero()));
+    let mut world = World {
+        ball: Ball::new(Point2::new(0.6, 0.)),
+        team: HashMap::new(),
+    };
+    world.team.insert(0, Robot::new(0, Point2::zero()));
+    world.team.insert(1, Robot::new(1, Point2::zero()));
+    world.team.insert(2, Robot::new(2, Point2::zero()));
 
-    let control_loop_thread = launch_control_thread(team.clone(), ball.clone());
+    let control_loop_thread = launch_control_thread(world.clone());
 
-    do_square(team.get(&0).unwrap()).await;
+    do_square(world.team.get(&0).unwrap()).await;
     // we simulate a penalty after 2s
     let _ = tokio::time::timeout(
         Duration::from_millis(500),
         three_attackers_attack(
-            team.get(&1).unwrap(),
-            team.get(&0).unwrap(),
-            team.get(&2).unwrap(),
+            world.team.get(&1).unwrap(),
+            world.team.get(&0).unwrap(),
+            world.team.get(&2).unwrap(),
         ),
     )
     .await;
 
     // now we spin the ball and make the robot try to go get it to showcase the Trackable trait
-    make_ball_spin(ball.clone(), Some(Duration::from_secs(5)));
-    go_get_ball(team.get(&0).unwrap(), &ball).await;
+    make_ball_spin(world.ball.clone(), Some(Duration::from_secs(5)));
+    go_get_ball(world.team.get(&0).unwrap(), &world.ball).await;
 
     control_loop_thread.abort();
 }
