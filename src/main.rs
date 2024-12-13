485a8af42f892d8a0ffc0cae385c1acc68f1de8d
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2024-12-13 16:53:52 +0100

    extract control loop

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod vec2;
 use robot::{Robot, RobotId};
 use std::{collections::HashMap, time::Duration};
 use strategies::{square::SquareStrategy, Strategy};
+use tokio::task::JoinHandle;
 use vec2::Vec2f32;
 
 const CONTROL_PERIOD: Duration = Duration::from_millis(50);
@@ -29,34 +30,33 @@ fn take_next_commands(robots: &mut HashMap<RobotId, Robot>) -> HashMap<RobotId, 
         .collect()
 }
 
+fn launch_control_thread(mut team: HashMap<RobotId, Robot>) -> JoinHandle<()> {
+    let mut interval = tokio::time::interval(CONTROL_PERIOD);
+    tokio::spawn(async move {
+        // Robot control loop, 1Hz
+        loop {
+            interval.tick().await; // first tick ticks immediately that's why it's at the beginning
+
+            dbg!(team.values().map(|r| r.get_pos()).collect::<Vec<Vec2f32>>());
+
+            println!("[DEBUG] sending commands!");
+            take_next_commands(&mut team)
+                .drain()
+                .for_each(|(rid, command)| {
+                    let r = team.get_mut(&rid).unwrap(); // TODO: check we have the robot to send the order to
+                    r.apply_vel(command.vel);
+                }); // here we would send that to the physical robots controller
+        }
+    })
+}
+
 /// Simulation of a real control loop
 #[tokio::main]
 async fn main() {
     let mut team: HashMap<RobotId, Robot> = HashMap::new();
     team.insert(0, Robot::new(0, Vec2f32::zero()));
 
-    let mut interval = tokio::time::interval(CONTROL_PERIOD);
-    let mut team_clone = team.clone();
-    let control_loop_thread = tokio::spawn(async move {
-        // Robot control loop, 1Hz
-        loop {
-            interval.tick().await; // first tick ticks immediately that's why it's at the beginning
-
-            dbg!(team_clone
-                .values()
-                .map(|r| r.get_pos())
-                .collect::<Vec<Vec2f32>>());
-
-            println!("[DEBUG] sending commands!");
-            take_next_commands(&mut team_clone)
-                .drain()
-                .for_each(|(rid, command)| {
-                    let r = team_clone.get_mut(&rid).unwrap(); // TODO: check we have the robot to send the order to
-                    r.apply_vel(command.vel);
-                }); // here we would send that to the physical robots controller
-        }
-    });
-
+    let control_loop_thread = launch_control_thread(team.clone());
     SquareStrategy.run(team).await;
 
     control_loop_thread.abort();
