658da4fb2435c900e9fd146cb1fcade88530d07c
Léo Paillé <leo.paille@enseirb-matmeca.fr>
2024-12-22 15:13:05 +0100

    feat: ctrl-c handler

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,65 @@
 use crabe_async::{
     actions::three_attackers_attack,
     controllers::sim_controller::SimRobotController,
+    game_controller::GameController,
     launch_control_thread,
     math::Point2,
     world::{AvoidanceMode, TeamColor, World},
     CONTROL_PERIOD,
 };
 use std::time::Duration;
-use tokio::{join, time::sleep};
+use tokio::{join, select, time::sleep};
+
+async fn play(world: World) {
+    let r0 = world.team.lock().unwrap().get(&3).unwrap().clone();
+    let r1 = world.team.lock().unwrap().get(&4).unwrap().clone();
+    let r2 = world.team.lock().unwrap().get(&5).unwrap().clone();
+    let ball = world.ball.clone();
+
+    let mut interval = tokio::time::interval(CONTROL_PERIOD);
+    loop {
+        interval.tick().await; // YIELD
+        let _ = r0
+            .goto(&world, &Point2::zero(), None, AvoidanceMode::AvoidRobots)
+            .await;
+        println!("r{} pos after move = {:?}", r0.get_id(), r0.get_pos());
+    }
+}
 
 /// Simulation of a real control loop
 #[tokio::main]
 async fn main() {
     let color = TeamColor::Blue;
     let world = World::default_with_team_color(color);
+    let gc = GameController::new(None, None);
     let controller = SimRobotController::new(color).await;
     let (control_loop_thread_stop_notifier, control_loop_thread_handle) =
         launch_control_thread(world.clone(), None, None, false, color, controller);
     sleep(CONTROL_PERIOD * 10).await; // AWAIT ROBOTS DETECTION
 
-    let r0 = world.team.lock().unwrap().get(&3).unwrap().clone();
-    let r1 = world.team.lock().unwrap().get(&4).unwrap().clone();
-    let r2 = world.team.lock().unwrap().get(&5).unwrap().clone();
-    let ball = world.ball.clone();
+    select! {
+        _ = play(world) => {}
+        _ = tokio::signal::ctrl_c() => {
+            println!("detected ctrl-c, stopping now!");
+        }
+    }
 
     // shoot(&world, &r0, &ball).await;
 
     // place_ball(&world, &r0, &ball, &goal).await;
-    // let _ = r0
-    //     .goto(&world, &Point2::zero(), None, AvoidanceMode::AvoidRobots)
-    //     .await;
-    r0.go_get_ball(&world, &ball).await;
-    three_attackers_attack(&world, &r1, &r0, &r2).await;
-    let (d1, d0, d2) = (
-        Point2::new(-1., 1.),
-        Point2::new(-1., 0.),
-        Point2::new(-1., -1.),
-    );
-    let _ = join!(
-        r1.goto(&world, &d1, Some(0.), AvoidanceMode::AvoidRobots,),
-        r0.goto(&world, &d0, Some(0.), AvoidanceMode::AvoidRobots,),
-        r2.goto(&world, &d2, Some(0.), AvoidanceMode::AvoidRobots,),
-    );
+
+    // r0.go_get_ball(&world, &ball).await;
+    // three_attackers_attack(&world, &r1, &r0, &r2).await;
+    // let (d1, d0, d2) = (
+    //     Point2::new(-1., 1.),
+    //     Point2::new(-1., 0.),
+    //     Point2::new(-1., -1.),
+    // );
+    // let _ = join!(
+    //     r1.goto(&world, &d1, Some(0.), AvoidanceMode::AvoidRobots,),
+    //     r0.goto(&world, &d0, Some(0.), AvoidanceMode::AvoidRobots,),
+    //     r2.goto(&world, &d2, Some(0.), AvoidanceMode::AvoidRobots,),
+    // );
 
     sleep(Duration::from_millis(100)).await;
     control_loop_thread_stop_notifier.notify_one(); // ask for stop
